a63bf49867897b0ae72d087f407de59aa290873f
katherin <katyflorezgo@gmail.com>
2025-03-12 21:11:39 -0500

    Se realizaron cambios al script de la prueba realizada con el archivo html

--- Entregable 02/script.js
+++ Entregable 02/script.js
@@ -1,40 +1,41 @@
 let suma
-let resta
-let multiplicacion
-let division
+        let resta
+        let multiplicacion
+        let division
     
-    function realizarOperacion(num1,num2,operacion) {
-    if(operacion === "suma"){
-        suma = num1 + num2
-        return suma
+        //Declaración de función para realizar operaciones
+        function realizarOperacion(num1,num2,operacion) {
+        if(operacion === "suma"){
+            suma = num1 + num2
+            return suma
 
-    }else if(operacion === "resta"){
-        resta = num1 - num2
-        return resta
+        }else if(operacion === "resta"){
+            resta = num1 - num2
+            return resta
 
-    }else if(operacion === "multiplicacion"){
-        multiplicacion = num1 * num2
-        return multiplicacion
+        }else if(operacion === "multiplicacion"){
+            multiplicacion = num1 * num2
+            return multiplicacion
 
-    }else if(operacion === "division"){
-        if(num2 === 0){
-            return division = "NO EXISTE"
+        }else if(operacion === "division"){
+            if(num2 === 0){
+                return division = "INEXISTENTE"
+            }else{
+                division = num1 / num2
+                return division
+            }
         }else{
-            division = num1 / num2
-            return division
+            return "ERRONEA"
+        } 
+        } 
+
+        let accion = prompt("Ingrese la operacion que desea realizar o escriba 'salir' para terminar el programa")
+
+        while(accion !== "salir"){
+            let x = parseFloat(prompt("Ingrese el PRIMER número"))
+            let y = parseFloat(prompt("Ingrese el SEGUNDO número"))
+            resultado = realizarOperacion(x,y,accion)
+            console.log("El resultado de la operación",accion,"de",x,"y",y,"es -> ",resultado)
+            accion = prompt("Diga la siguiente operación a realizar o escriba 'salir' para terminar el programa") 
         }
-    }else{
-        console.log("Error!!! Esa opción no existe")
-    }   
-    } 
-
-    let accion = prompt("Ingrese la operacion que desea realizar o escriba 'salir' para terminar el programa")
-
-    while(accion !== "salir"){
-        let x = parseFloat(prompt("Ingrese el PRIMER número"))
-        let y = parseFloat(prompt("Ingrese el SEGUNDO número"))
-        resultado = realizarOperacion(x,y,accion)  //Llamado de la funcion
-        console.log("El resultado de la ",accion," de ",x," y ",y," es -> ",resultado)
-        accion = prompt("Desea realizar otra operacion o Escriba 'salir' para terminar el programa")
-    }
-    console.log("El programa se detuvo satisfactoriamente. Hasta Pronto!!")
+        console.log("El programa se detuvo satisfactoriamente. Hasta Pronto!!")
